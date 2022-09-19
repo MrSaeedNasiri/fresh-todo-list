fdf2bdafaad27fe79af0684dafc2a65374f32c56
MrSaeedNasiri <mr.nasiri.saeed@gmail.com>
2022-09-19 08:30:36 +0430

    add fresh badge and project url

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# fresh-todo-list example
+# fresh-todo-list
+[![Made with Fresh](https://fresh.deno.dev/fresh-badge.svg)](https://fresh.deno.dev)
 
 ### Usage
 
@@ -9,3 +10,9 @@ deno task start
 ```
 
 This will watch the project directory and restart as necessary.
+
+## Preview
+
+```
+https://fresh-todo-list.deno.dev/
+```
--- routes/index.tsx
+++ routes/index.tsx
@@ -8,6 +8,9 @@ export default function Home() {
         class="w-32 h-32"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
+      <a href="https://fresh.deno.dev">
+        <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge.svg" alt="Made with Fresh" />
+      </a>
       <h1 class="text-3xl p-2">
         Fresh ToDo List
       </h1>
