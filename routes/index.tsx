5cc452953f064b6b1dd0534ee8800f6fa7e2a1ee
xyntechx <xyntechx@gmail.com>
2022-09-19 16:23:52 +0800

    Add Head tag

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,49 +1,56 @@
+import { Head } from "$fresh/runtime.ts";
 import Todo from "../islands/Todo.tsx";
 
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md flex flex-col justify-center items-center">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <a href="https://fresh.deno.dev">
+    <div>
+      <Head>
+        <title>Fresh ToDo List</title>
+        <meta name="description" content="Fresh ToDo List" />
+      </Head>
+      <main class="p-4 mx-auto max-w-screen-md flex flex-col justify-center items-center">
         <img
-          width="197"
-          height="37"
-          src="https://fresh.deno.dev/fresh-badge.svg"
-          alt="Made with Fresh"
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
         />
-      </a>
-      <a
-        class="absolute top-10 right-10 text-gray-700 hover:text-black fill-current"
-        href="https://github.com/MrSaeedNasiri/fresh-todo-list"
-        data-hotkey="g d"
-        aria-label="Homepage "
-        data-turbo="false"
-        data-analytics-event="{&quot;category&quot;:&quot;Header&quot;,&quot;action&quot;:&quot;go to dashboard&quot;,&quot;label&quot;:&quot;icon:logo&quot;}"
-      >
-        <svg
-          height="32"
-          aria-hidden="true"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="32"
-          data-view-component="true"
-          class=""
+        <a href="https://fresh.deno.dev">
+          <img
+            width="197"
+            height="37"
+            src="https://fresh.deno.dev/fresh-badge.svg"
+            alt="Made with Fresh"
+          />
+        </a>
+        <a
+          class="absolute top-10 right-10 text-gray-700 hover:text-black fill-current"
+          href="https://github.com/MrSaeedNasiri/fresh-todo-list"
+          data-hotkey="g d"
+          aria-label="Homepage "
+          data-turbo="false"
+          data-analytics-event="{&quot;category&quot;:&quot;Header&quot;,&quot;action&quot;:&quot;go to dashboard&quot;,&quot;label&quot;:&quot;icon:logo&quot;}"
         >
-          <path
-            fill-rule="evenodd"
-            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          <svg
+            height="32"
+            aria-hidden="true"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="32"
+            data-view-component="true"
+            class=""
           >
-          </path>
-        </svg>
-      </a>
-      <h1 class="text-3xl p-2">
-        Fresh ToDo List
-      </h1>
-      <Todo />
+            <path
+              fill-rule="evenodd"
+              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+            >
+            </path>
+          </svg>
+        </a>
+        <h1 class="text-3xl p-2">
+          Fresh ToDo List
+        </h1>
+        <Todo />
+      </main>
     </div>
   );
 }
